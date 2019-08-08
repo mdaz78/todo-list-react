db60a4f7302853a7beae01689381e2fa983d94cf
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-08-08 12:30:05 +0530

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Clean the house',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Learn React',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Learn DS and Algo',
+        completed: false,
+      },
+    ],
+  };
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  deleteTodo = (id) => {
+    console.log('clicked');
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          deleteTodo={this.deleteTodo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      backgroundColor: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+    };
+  };
+
+  render() {
+    const { title, id } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type='checkbox'
+            onChange={this.props.markComplete.bind(this, id)}
+          />{' '}
+          {title}
+          <button
+            onClick={this.props.deleteTodo.bind(this, id)}
+            style={btnDeleteStyle}
+          >
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+};
+
+const btnDeleteStyle = {
+  background: '#ff0000',
+  float: 'right',
+  border: 'none',
+  padding: '5px 9px',
+  color: '#fff',
+  fontSize: '11px',
+  cursor: 'pointer',
+  borderRadius: '50%',
+  fontWeight: 'bold',
+};
+
+export default TodoItem;
